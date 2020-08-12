38c1529eff975be3868268cebed473d58292c948
Dhruv Desai <46900142+dhruv1214@users.noreply.github.com>
2020-08-12 20:56:53 +0530

    Add files via upload

---
+++ basic.ws.kts
@@ -0,0 +1,11 @@
+import java.lang.Integer as Integer1
+
+fun main(args: Array<String>)
+{
+    println(message = """Enter your name""")
+    val name = readLine()
+    println("enter your age")
+    var age : Int
+    age = Integer.Valueof(readLine())
+    println("Your name is $name and your age is $age")
+}
